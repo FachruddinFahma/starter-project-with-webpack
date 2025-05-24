f9e966786249a7aa1717b1fb9146cd7149b8a20f
fachruddin <118855404+FachruddinFahma@users.noreply.github.com>
2025-05-24 23:55:29 +0700

    FIx bug in manifest part 3

--- webpack.common.js
+++ webpack.common.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/", // ← ini buat base path
+    // publicPath: "/", // ← ini buat base path
   },
   module: {
     rules: [
